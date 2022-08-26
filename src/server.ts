d01f201ec4891c09007085f98ccd00f2e79b3f33
Alfred Nyambane <nyambanealfred1539@gmail.com>
2022-08-27 01:51:18 +0300

    created api to filted image from url on server.ts file

--- src/server.ts
+++ src/server.ts
@@ -35,12 +35,12 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get("/filteredimage", async (req:Request, res: Response) => {
     const { image_url } = req.query.image_url.toString();
     if ( !image_url ) {
-        return res.status(400).send("valid Image url is needed");
+        res.status(400).send("valid Image url is needed");
       }
        
     const filtered_image = await filterImageFromURL(image_url);
 
-    return res.status(200).sendFile(filtered_image, () => {
+        res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image])
 
     });
